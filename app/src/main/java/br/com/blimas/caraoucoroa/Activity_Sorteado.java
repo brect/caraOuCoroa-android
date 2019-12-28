db3c80468b09c2c15ba2662c9f2f9f8973f0762e
brect <b.limas93@gmail.com>
2019-12-27 23:59:07 -0300

    Metodos criados;

--- app/src/main/java/br/com/blimas/caraoucoroa/Activity_Sorteado.java
+++ app/src/main/java/br/com/blimas/caraoucoroa/Activity_Sorteado.java
@@ -3,12 +3,41 @@ package br.com.blimas.caraoucoroa;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class Activity_Sorteado extends AppCompatActivity {
+
+    private Button botaoVoltar;
+    private ImageView exibeMoedaSorteada;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sorteado);
+
+        botaoVoltar = findViewById(R.id.btn_voltar);
+        exibeMoedaSorteada = findViewById(R.id.moeda_sorteada);
+
+        //recuperar dados da mainactivity
+        Bundle dadosRecebidos = getIntent().getExtras();
+        int numeroSorteado = dadosRecebidos.getInt("numero");
+
+        if(numeroSorteado == 0){
+            exibeMoedaSorteada.setImageResource(R.drawable.moeda_cara);
+        }else {
+            exibeMoedaSorteada.setImageResource(R.drawable.moeda_coroa);
+        }
+
+        //evento de clique para o botao voltar
+        botaoVoltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //finaliza activity_sorteado
+                finish();
+            }
+        });
     }
 }
--- app/src/main/java/br/com/blimas/caraoucoroa/MainActivity.java
+++ app/src/main/java/br/com/blimas/caraoucoroa/MainActivity.java
@@ -2,13 +2,35 @@ package br.com.blimas.caraoucoroa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button botaoJogar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        botaoJogar = findViewById(R.id.btn_jogar);
+
+        botaoJogar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent activityMoedaSorteada = new Intent(getApplicationContext(), Activity_Sorteado.class);
+
+                //passa valores para a outra activity
+                int numeroSorteado = new Random().nextInt(2);
+                activityMoedaSorteada.putExtra("numero", numeroSorteado);
+
+                startActivity(activityMoedaSorteada);
+            }
+        });
     }
 }
