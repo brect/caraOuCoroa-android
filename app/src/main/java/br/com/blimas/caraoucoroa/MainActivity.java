fea1785fe9186daae8aff4d4f6921b50c80c37c7
brect <b.limas93@gmail.com>
2019-12-27 23:34:55 -0300

    Layout cara ou coroa criado.

---
+++ app/src/main/java/br/com/blimas/caraoucoroa/MainActivity.java
@@ -0,0 +1,14 @@
+package br.com.blimas.caraoucoroa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
